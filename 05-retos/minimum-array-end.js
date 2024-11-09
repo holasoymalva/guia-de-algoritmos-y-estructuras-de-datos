057ba67ae0c7c7eb996fdd5da2746a60f3fc25cc
Leon Martin <13058234+holasoymalva@users.noreply.github.com>
2024-11-09 02:23:31 -0600

    Update minimum-array-end.js

--- 05-retos/minimum-array-end.js
+++ 05-retos/minimum-array-end.js
@@ -8,6 +8,15 @@
 //     return a * 2 - 1;
 // }
 
+// var minEnd = function(n, x) {
+//     let num = x;
+
+//     for(let i = 1; i < n; i ++){
+//         num = (num | x) + 1;
+//     }
+//     return num - 1;
+// };
+
 var minEnd = function(n, x) {
     let num = x;
 
