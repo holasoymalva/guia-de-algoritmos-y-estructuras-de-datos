291848b2419a7ff73392b4139d14af5ebeed5617
Leon Martin <13058234+holasoymalva@users.noreply.github.com>
2024-11-09 02:23:04 -0600

    Create minimum-array-end.js

---
+++ 05-retos/minimum-array-end.js
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/minimum-array-end
+
+// var minEnd = function (n, x) {
+//     let a = 1;
+//     for (let i = 1; i < n - 1; i++) {
+//         a = a * 2;
+//     }
+//     return a * 2 - 1;
+// }
+
+var minEnd = function(n, x) {
+    let num = x;
+
+    for(let i = 1; i < n; i ++){
+        num++;
+        while ((num & x) !== x){
+            num++;
+        }
+    }
+    return num;
+};
