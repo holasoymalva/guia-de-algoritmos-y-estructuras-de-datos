303b374c6998357a0bd1e870447026295f9279a4
Leon Martin <13058234+holasoymalva@users.noreply.github.com>
2025-08-05 06:20:44 -0700

    Update martin-challenge.js

--- 05-code-challenges/august-challenge/challenge-05/martin-challenge.js
+++ 05-code-challenges/august-challenge/challenge-05/martin-challenge.js
@@ -1,1 +1,39 @@
+/*
+You are given two arrays of integers, fruits and baskets, each of length n, where fruits[i] represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+From left to right, place the fruits according to these rules:
 
+Each fruit type must be placed in the leftmost available basket with a capacity greater than or equal to the quantity of that fruit type.
+Each basket can hold only one type of fruit.
+If a fruit type cannot be placed in any basket, it remains unplaced.
+Return the number of fruit types that remain unplaced after all possible allocations are made.
+
+Example 1:
+Input: fruits = [4,2,5], baskets = [3,5,4]
+Output: 1
+
+Explanation:
+fruits[0] = 4 is placed in baskets[1] = 5.
+fruits[1] = 2 is placed in baskets[0] = 3.
+fruits[2] = 5 cannot be placed in baskets[2] = 4.
+Since one fruit type remains unplaced, we return 1.
+*/
+
+var numOfUnplacedFruits = function(fruits, baskets) {
+    const n = fruits.length;
+    const used = new Array(n).fill(false);
+    let unplaced = 0;
+    for(let i = 0; i < n; i++){
+        let placed = false;
+        for(let j = 0; j < n; j++){
+            if (!used[j] && baskets[j] >= fruits[i]) {
+                used[j] = true;
+                placed = true;
+                break;
+            }
+        }
+        if (!placed) {
+            unplaced++;
+        }
+    }
+    return unplaced;
+};
